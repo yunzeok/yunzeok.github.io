5040a54f673749627c090a857ea4569e2d06307e
yunze <yunzhongmengze@gmail.com>
2023-08-18 23:01:31 +0800

    Update frp.sh

--- sh/frp.sh
+++ sh/frp.sh
@@ -35,8 +35,6 @@ if ! command -v systemctl &> /dev/null; then
         exit 1
     fi
 fi
-echo "本脚本推荐本地安装，请自行前往https://github.com/fatedier/frp/releases/中下载linux版本文件，将其改名为frp.tar.gz并存放在用户文件夹 $HOME 下"
-echo "本脚本提供网络安装，但是安装的版本为v0.51.3"
 echo "如已经安装过，请提前做好配置文件的备份"
 # 获取用户选择是安装frpc还是frps
 echo "请选择要安装的组件："
@@ -72,10 +70,13 @@ if [ ! -f "$FRP_PACKAGE_PATH" ]; then
     case $download_choice in
         1)
             echo "请手动下载 frp.tar.gz 并放置在 $FRP_PACKAGE_PATH"
+            echo "请自行前往https://github.com/fatedier/frp/releases/中下载linux版本文件，将其改名为frp.tar.gz并存放在用户文件夹 $HOME 下"
+            echo "请下载对应的版本，否则可能无法正常使用"
             exit 1
             ;;
         2)
             echo "正在从网络下载..."
+            echo "本脚本所提供的网络安装（定时自动拉取最新版），安装版本可能落后于github版本"
             curl -o "$FRP_PACKAGE_PATH" "https://hub.yunzeofficial.cn/download/frp.tar.gz"
             ;;
         *)
