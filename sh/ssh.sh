6c63d7212be5bfa97fdf5d54279cb781b41bbd12
yunze <97683172+yunzeo@users.noreply.github.com>
2024-09-09 23:40:21 +0800

    Update ssh.sh

--- sh/ssh.sh
+++ sh/ssh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 指定公钥内容，替换为你自己的公钥
-PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDA6P97uQ7S2J8TR919GJpRrHlMKsCSOFlCK8WVaZNSmf+huS8oxQVuz8N12MsRD2Kog42B9gS2ymmwbmXl/iV4xJ3YVmv/NkCAb2B1ZoK+Zj2c3/6Jdlu6HcSH4cSJhY5gLOk4ac5tPFFKkpwNe95p7whinkNrSGNV143wfmZDZs9MDYFhmfZwKsLoQi6PQGtivixa1uwZDw0ziUiC+JfWQXFBGOuX3pUB2hY+zJECYx2cK9p+LfF+vgkVxkLXyTvpbSnnfpIaeFL4em8a7PGBaoJysuGG0MuqiaCMB/eP7BNlEEi5Ryud8ci8+Akgl+3AyXjSUqzkUyhQiJ+xy4AP9JM5O5dNZY0poTINvfPd1VEs5BRusakJoIc9uS1W2WHlSF4rK3CpId4iq+ZH2UHQV4584UlT97snAtPjVehSuvfTRj5mf4bl8nhrNs9d6Oy/IsOP379iyxsfGE5iVXrGjj6J9KoCTn76Y2aqvnLRbtIlPFbO3K6A6SG2BWs/g00="
+# 指定公钥内容
+PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDA6P97uQ7S2J8TR919GJpRrHlMKsCSOFlCK8WVaZNSmf+huS8oxQVuz8N12MsRD2Kog42B9gS2ymmwbmXl/iV4xJ3YVmv/NkCAb2B1ZoK+Zj2c3/6Jdlu6HcSH4cSJhY5gLOk4ac5tPFFKkpwNe95p7whinkNrSGNV143wfmZDZs9MDYFhmfZwKsLoQi6PQGtivixa1uwZDw0ziUiC+JfWQXFBGOuX3pUB2hY+zJECYx2cK9p+LfF+vgkVxkLXyTvpbSnnfpIaeFL4em8a7PGBaoJysuGG0MuqiaCMB/eP7BNlEEi5Ryud8ci8+Akgl+3AyXjSUqzkUyhQiJ+xy4AP9JM5O5dNZY0poTINvfPd1VEs5BRusakJoIc9uS1W2WHlSF4rK3CpId4iq+ZH2UHQV4584UlT97snAtPjVehSuvfTRj5mf4bl8nhrNs9d6Oy/IsOP379iyxsfGE5iVXrGjj6J9KoCTn76Y2aqvnLRbtIlPFbO3K6A6SG2BWs/g00=$(whoami)@$(hostname)"
 
 # 确保 .ssh 目录存在
 if [ ! -d ~/.ssh ]; then
@@ -27,9 +27,8 @@ if [ ! -f "$SSH_CONFIG_FILE.bak" ]; then
     echo "已备份 $SSH_CONFIG_FILE 到 $SSH_CONFIG_FILE.bak"
 fi
 
-# 确保配置允许公钥登录
-sed -i 's/#*PubkeyAuthentication no/PubkeyAuthentication yes/' $SSH_CONFIG_FILE
-sed -i 's/#*PermitRootLogin yes/PermitRootLogin no/' $SSH_CONFIG_FILE
+# 确保 PermitRootLogin 为 yes
+#sed -i 's/#*PermitRootLogin no/PermitRootLogin yes/' $SSH_CONFIG_FILE
 
 # 选择是否禁用密码登录
 echo "是否禁用密码登录？（默认不禁用）"
